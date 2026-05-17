d99cb749da9ec8f9a0ad05384d90d21f83e10a13
tabosann <tabosann@example.com>
2026-05-18 06:11:33 +0900

    [EDT]ps1スクリプトを叩くだけでアーカイブ化できるように

--- PasswordGeneratorTabo/Compress-Archive.ps1
+++ PasswordGeneratorTabo/Compress-Archive.ps1
@@ -1,4 +1,4 @@
-pushd 'bin\x64\Release\net10.0-windows10.0.26100.0\win-x64\publish\win-x64'
-Get-ChildItem -Directory -Exclude 'en-us','ja-JP' | ?{ Test-Path "$_\Microsoft.ui.xaml.dll.mui" } | rm -Recurse
-Compress-Archive -Path '.\*' -DestinationPath '..\..\..\..\net10.0-windows10.0.26100.0-win-x64.zip'
+dotnet publish -p:PublishProfile=NoNativeAOT
+pushd 'bin\x64\Release_Unpackaged\net10.0-windows10.0.26100.0\win-x64\publish\PasswordGeneratorTabo'
+Compress-Archive -Path '.\*' -DestinationPath '..\..\..\..\net10.0-windows10.0.26100.0-win-x64.zip' -Force
 popd
